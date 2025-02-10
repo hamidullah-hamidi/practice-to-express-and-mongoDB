c55710002e8d20523c9312e66292b555c6e36734
Hamidullah <hamidullahh754@gmail.com>
2025-02-09 19:26:40 -0800

    u

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-// mongoose.connect()
+mongoose.connect(
+  'mongodb+srv://hamidullah:<079857ha>@cluster0.tvzu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  {
+    useNewUrlParser: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on('error', (err) => console.error(err));
+db.once('open', () => console.log('Connected to database'));
 
 app.listen(3000, () => console.log('Server Started'));
-
-console.log('Hi hamid');
