44a7c9871296864dd01da2e56c27b81319d04188
Hamid <hamidullahh754@gmail.com>
2025-02-09 21:45:19 -0800

    u

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Subscribers = require('../model/subscriber');
 
 // Get all
-router.get('/', (req, res) => {
-  res.send('hi from server');
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscribers.find();
+    res.status(200).json({
+      subscribers,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
 // Get one
-router.get('/:id', (req, res) => {
-  res.send(req.params.id);
+router.get('/:id', async (req, res) => {
+  const subscriber = new Subscribers({
+    name: req.body.name,
+    subscribeChannel: req.body.subscribeChannel,
+    subscriberDate: req.body.subscriberDate,
+  });
+
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json({
+      newSubscriber,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
 // Create one
