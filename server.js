d23f56d9ce3dac445fdc3d06a842bb8d24441fe6
Hamidullah <hamidullahh754@gmail.com>
2025-02-09 19:03:26 -0800

    prepare project for practice

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+// mongoose.connect()
+
+app.listen(3000, () => console.log('Server Started'));
+
+console.log('Hi hamid');
