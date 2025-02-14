9f48dde957e2e5d4beab862938dfc9ceac469916
Hamidullah Hamidi <hamidullahh754@gmail.com>
2025-02-14 18:38:03 +0430

    u

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -17,7 +17,22 @@ router.get('/', async (req, res) => {
 });
 
 // Get one
-router.get('/:id', async (req, res) => {});
+router.get('/:id', async (req, res) => {
+  try {
+    const subscriber = await Subscribers.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      subscriber: {
+        subscriber,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
 
 // Create one
 router.post('/', async (req, res) => {
@@ -35,9 +50,38 @@ router.post('/', async (req, res) => {
 });
 
 // Update one
-router.patch('/:id', (req, res) => {});
+router.patch('/:id', async (req, res) => {
+  try {
+    const updatedSubscriber = await Subscribers.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.status(200).json({
+      updatedSubscriber: { updatedSubscriber },
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
 
 // Delete one
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedSubscriber = await Subscribers.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      deletedSubscriber: {
+        deletedSubscriber,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
 
 module.exports = router;
